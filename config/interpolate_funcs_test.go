6ef10e350e7e95f9f6ea95d4d6d025219450d669
Vladislav Rassokhin <vladislav.rassokhin@jetbrains.com>
2015-10-24 05:46:00 +0300

    Initial version of interpolation functions schema export (using 'functions' argument for 'schemas' command)
    Fix compilation
    
    Fix test compilation
    
    simplify functions schema

--- config/interpolate_funcs_test.go
+++ config/interpolate_funcs_test.go
@@ -2438,7 +2438,7 @@ func TestInterpolateFuncUUID(t *testing.T) {
 			t.Fatalf("err: %s", err)
 		}
 
-		result, err := hil.Eval(ast, langEvalConfig(nil))
+		result, err := hil.Eval(ast, LangEvalConfig(nil))
 		if err != nil {
 			t.Fatalf("err: %s", err)
 		}
@@ -2458,7 +2458,7 @@ func TestInterpolateFuncTimestamp(t *testing.T) {
 		t.Fatalf("err: %s", err)
 	}
 
-	result, err := hil.Eval(ast, langEvalConfig(nil))
+	result, err := hil.Eval(ast, LangEvalConfig(nil))
 	if err != nil {
 		t.Fatalf("err: %s", err)
 	}
@@ -2524,7 +2524,7 @@ func testFunction(t *testing.T, config testFunctionConfig) {
 				t.Fatalf("unexpected parse error: %s", err)
 			}
 
-			result, err := hil.Eval(ast, langEvalConfig(config.Vars))
+			result, err := hil.Eval(ast, LangEvalConfig(config.Vars))
 			if err != nil != tc.Error {
 				t.Fatalf("unexpected eval error: %s", err)
 			}
@@ -2641,7 +2641,7 @@ func TestInterpolateFuncBcrypt(t *testing.T) {
 		t.Fatalf("err: %s", err)
 	}
 
-	result, err := hil.Eval(node, langEvalConfig(nil))
+	result, err := hil.Eval(node, LangEvalConfig(nil))
 	if err != nil {
 		t.Fatalf("err: %s", err)
 	}
